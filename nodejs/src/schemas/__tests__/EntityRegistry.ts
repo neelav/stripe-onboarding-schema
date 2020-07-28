f592964c6e9ea88eea60f96742e7132f4ba587b9
Neelav Rana <neelav@duettoresearch.com>
2020-07-28 16:46:43 -0700

    some more tests

--- nodejs/src/schemas/__tests__/EntityRegistry.ts
+++ nodejs/src/schemas/__tests__/EntityRegistry.ts
@@ -1,3 +1,4 @@
+import Entity from 'schema-core/Entity';
 import EntityRegistry from '../EntityRegistry';
 
 describe('EntityRegistry', () => {
@@ -5,5 +6,21 @@ describe('EntityRegistry', () => {
     const registry = EntityRegistry.makeDefault();
     const businessTypeField = registry.lookupField('account', 'business_type');
     expect(businessTypeField).toBeTruthy();
+  });
+
+  test('failed lookups', () => {
+    const registry = EntityRegistry.makeDefault();
+    expect(registry.lookupField('bogus', 'business_type')).toBeUndefined();
+    expect(registry.lookupField('account', 'bogus')).toBeUndefined();
+  });
+
+  test('lookupEntityByToken', () => {
+    const registry = EntityRegistry.makeDefault();
+    expect(registry.lookupEntityByToken('person_123')).toBeUndefined();
+  });
+
+  test('validations', () => {
+    const schema = new Entity('test', 'test', 'test', []);
+    expect(() => new EntityRegistry([schema, schema])).toThrowError('Found duplicate entity names: test');
   });
 });
