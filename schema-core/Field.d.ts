d324a1ffc7bf41907f94f734721fb82e7bf30e46
Neelav Rana <neelav@duettoresearch.com>
2020-08-07 16:43:48 -0700

    Deploying version release-0.0.16

---
+++ schema-core/Field.d.ts
@@ -0,0 +1,24 @@
+import Stripe from 'stripe';
+import FieldType from './fieldtypes/FieldType';
+import EnumAttributes from './fieldtypes/EnumAttributes';
+import TextAttributes from './fieldtypes/TextAttributes';
+export declare type Container = Stripe.Account | Stripe.Person;
+declare type Attributes = EnumAttributes | TextAttributes;
+declare class Field<C extends Container, V> {
+    readonly id: string;
+    readonly name: string;
+    readonly description: string;
+    readonly fieldType: FieldType;
+    readonly setValue: (container: C, value: V) => void;
+    readonly getValue: (container: C) => V | null | undefined;
+    readonly attributes?: Attributes;
+    constructor(id: string, name: string, description: string, fieldType: FieldType, setValue: (container: C, value: V) => void, getValue: (container: C) => V | null | undefined, attributes?: Attributes);
+    private static EMPTY_SETTER;
+    private static EMPTY_GETTER;
+    /**
+     * Use this method when you want to advertise to the UI that the particular id could not be
+     * resolved to a "real" field.
+     */
+    static unknown(id: string): Field<any, any>;
+}
+export default Field;
---
+++ types/types.d.ts
@@ -0,0 +1,23 @@
+import Field from '../schema-core/Field';
+export declare enum EntityType {
+    ACCOUNT = "ACCOUNT",
+    UNKNOWN = "UNKNOWN"
+}
+export declare enum RequirementsType {
+    PAST_DUE = "past_due",
+    CURRENTLY_DUE = "currently_due",
+    EVENTUALLY_DUE = "eventually_due"
+}
+export declare enum Country {
+    US = "US",
+    CA = "CA",
+    GB = "GB",
+    FR = "FR"
+}
+export declare class Requirement {
+    readonly requirementId: string;
+    readonly entityType: EntityType;
+    readonly entityToken?: string;
+    readonly field: Field<any, any>;
+    constructor(requirementId: string, entityType: EntityType, field: Field<any, any>, entityToken?: string);
+}
