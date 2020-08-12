c6586a23b09fdf8242851ad1854aea151fd1b9f0
Neelav Rana <neelav@duettoresearch.com>
2020-08-12 10:17:07 -0700

    Deploying version release-0.0.20

---
+++ schema-core/fieldtypes/EnumAttributes.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class EnumAttributes {
+    constructor(values) {
+        this.values = values;
+    }
+}
+exports.default = EnumAttributes;
---
+++ schema-core/fieldtypes/FieldType.d.ts
@@ -0,0 +1,13 @@
+declare enum FieldType {
+    TEXT = "TEXT",
+    BOOLEAN = "BOOLEAN",
+    DOCUMENT = "DOCUMENT",
+    NUMBER = "NUMBER",
+    ENUM = "ENUM",
+    ADDRESS = "ADDRESS",
+    URL = "URL",
+    PHONE = "PHONE",
+    EMAIL = "EMAIL",
+    UNKNOWN = "UNKNOWN"
+}
+export default FieldType;
